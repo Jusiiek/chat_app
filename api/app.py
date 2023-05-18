88a03c3984466aa5001a554a67f7338e64266e4a
Jakub Zurawski <jakub.zurawski@limebrains.com>
2023-05-18 20:59:28 +0200

    Added mongo db connector

--- api/app.py
+++ api/app.py
@@ -4,12 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api.config.enviroments import WEB_APP_ADDRESS
 from api.config.app_middlewares import headers_middleware
+from api.config.mongo_config import close_db, connect_db
 
 
 def create_app():
 	app = FastAPI()
+	connect_db()
+	app.add_event_handler("shutdown", close_db)
 	app.middleware('http')(headers_middleware)
-
 	app.add_middleware(
 		CORSMiddleware,
 		allow_origins=[WEB_APP_ADDRESS],
--- api/config/enviroments.py
+++ api/config/enviroments.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 WEB_APP_ADDRESS = os.environ['WEB_APP_ADDRESS']
+MONGO_URL = os.environ['MONGO_URL']
