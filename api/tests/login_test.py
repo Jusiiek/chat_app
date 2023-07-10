cd539ee04abb6700faedd502d647e71612f13999
Jakub Zurawski <jakub.zurawski@limebrains.com>
2023-07-10 22:26:08 +0200

    updated tests

--- api/tests/login_test.py
+++ api/tests/login_test.py
@@ -12,35 +12,50 @@ JWT_ENDPOINTS = {
 	'register_endpoint': '/api/jwt/register/'
 }
 
-#TODO register and login still not working
-def login_test():
+class TestJWT:
+	def test_login(self):
+		user_data = {
+			"username": "great_admin",
+			"password": "T3stC00mon"
+		}
+
+		res = app_client.post(
+			JWT_ENDPOINTS['login_endpoint'],
+			data=user_data
+		)
+
+		assert res.status_code == 200
+
+
+	def test_register(self):
+		user_data = {
+			"email": "test@test.com",
+			"username": "test_from_test",
+			"password": "12zaqWSX!@",
+			"re_password": "12zaqWSX!@"
+		}
+
+		res = app_client.post(
+			JWT_ENDPOINTS['register_endpoint'],
+			data=user_data
+		)
+
+		assert res.status_code == 200
+
+
+@pytest.fixture
+def client():
 	user_data = {
 		"username": "great_admin",
 		"password": "T3stC00mon"
 	}
-
 	res = app_client.post(
 		JWT_ENDPOINTS['login_endpoint'],
 		data=user_data
 	)
-
-	print(res)
-	assert res.status_code == 200
-
-
-def register_test():
-	user_data = {
-		"email": "test@test.com",
-		"username": "test_from_test",
-		"password": "12zaqWSX!@",
-		"re_password": "12zaqWSX!@"
-	}
-
-	res = app_client.post(
-		JWT_ENDPOINTS['register_endpoint'],
-		data=user_data
+	data = res.json()
+	app_client.headers.update(
+		{"Authorization": f"{data['token_type']} {data['token']}"}
 	)
 
-	print(res)
-	assert res.status_code == 200
-	data = res.json()
+	return client
