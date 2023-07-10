67a964915141764a4c14701b914b5b422a831303
Jakub Zurawski <jakub.zurawski@limebrains.com>
2023-07-10 20:30:50 +0200

    created users endpoints

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -2,12 +2,24 @@ from fastapi import APIRouter, Depends
 
 from dependencies.users import get_current_user
 
+from models.cassandra.users import User
+
 router = APIRouter(
 	prefix="/api/user",
 	tags=['users'],
+	dependencies=[Depends(get_current_user)]
 )
 
 
 @router.get("/")
-def get_users(user: Depends(get_current_user)):
-	print("User", user)
+def get_users():
+	users_list = User.objects.all()
+	return users_list
+
+
+@router.delete("/{user_id}/")
+def delete_user(user_id: str):
+	user_delete = User.objects.filter(user_id=user_id).first()
+	user_delete.delete()
+
+	return {}
